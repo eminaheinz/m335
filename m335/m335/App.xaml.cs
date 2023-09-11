34ec651117f7cae0db62aa037476c72f715e4aa4
emina <eminaheinz@icloud.com>
2023-09-11 08:37:11 +0200

    Start of Project

---
+++ m335/m335/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace m335
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ m335/m335/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace m335
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
