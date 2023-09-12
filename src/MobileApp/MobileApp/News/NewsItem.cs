c986d7101c3d8250efad4634df878dc53ce80b96
emina <eminaheinz@icloud.com>
2023-09-12 12:54:14 +0200

    mittag 12.09

---
+++ src/MobileApp/MobileApp/News/NewsItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileApp.News
+{
+    public class NewsItem
+    {
+        public string Headline { get; set; }
+        public string Content { get; set; }
+        public string Date { get; set; }
+    }
+}
--- src/MobileApp/MobileApp/News/NewsPage.xaml.cs
+++ src/MobileApp/MobileApp/News/NewsPage.xaml.cs
@@ -1,9 +1,47 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using Microsoft.Maui.Controls;
+
 namespace MobileApp.News;
 
 public partial class NewsPage : ContentPage
 {
-	public NewsPage()
-	{
-		InitializeComponent();
-	}
+    public NewsPage()
+    {
+        InitializeComponent();
+        LoadMauiAsset().ContinueWith((task) =>
+        {
+            var newsItems = task.Result;
+
+            if (newsItems != null && newsItems.Count > 0)
+            {
+                var firstNewsItem = newsItems[0];
+                newsHeadlineLabel.Text = firstNewsItem.Headline;
+                newsContentLabel.Text = firstNewsItem.Content;
+                newsDateLabel.Text = firstNewsItem.Date;
+            }
+        }, TaskScheduler.FromCurrentSynchronizationContext());
+    }
+    async Task<List<NewsItem>> LoadMauiAsset()
+    {
+        try
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("news.json");
+            using var reader = new StreamReader(stream);
+    
+            var content = await reader.ReadToEndAsync();
+            var newsItems = JsonSerializer.Deserialize<List<NewsItem>>(content);
+
+            
+            return newsItems;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return null;
+        }
+    }
+
+
 }
